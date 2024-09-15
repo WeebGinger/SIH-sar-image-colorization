8991bae42ab44e262e6fccdeaf1f35ed043652f4
WeebGinger <gingerbit.ly2@gmail.com>
2024-09-15 19:50:05 +0530

    setup-2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(path:str)->List[str]:
         requirements =  [req.replace('\n','') for req in requirements] 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    return requirements
+    return requirements 
 
 setup(
     name='sar-colorization',
@@ -25,5 +25,3 @@ setup(
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
-
-
